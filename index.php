28ff5f9a3265a70f9ca92e2c26ff73e636239953
laras126 <laras126@gmail.com>
2015-10-03 16:07:08 -0400

    add files

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php 
+	
+	$page = $_GET['page'];
+
+	if ($page == 'about') {
+		$page_title = "About";
+	} else {
+		$page_title = "NOT Tooooosday!";
+	}
+
+ ?>
+
+
+<?php include('partials/header.php'); ?>
+
+asldjalsjdlasjdlakjsdljkasldj
+
+<?php include('partials/footer.php'); ?>
+	
---
+++ partials/footer.php
@@ -0,0 +1,5 @@
+
+	<p>Copyright hello nice</p>
+
+</body>
+</html>
